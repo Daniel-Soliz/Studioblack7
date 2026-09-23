71e9e1f9c37e2c8b2091f3e7ad695450e3bba15e
Daniel-Soliz <danielsoliz1973@gmail.com>
2026-09-23 15:08:56 -0300

    Show install option immediately on site entry

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AuthProvider, StoreProvider, CartProvider } from './context';
+import { InstallAppModal } from './components/InstallAppModal';
 
 // Public Pages
 import {
@@ -52,6 +53,7 @@ export default function App() {
         <CartProvider>
           <BrowserRouter basename={import.meta.env.BASE_URL}>
             <ScrollToTop />
+            <InstallAppModal />
             <Routes>
               {/* Public Website Routes */}
               <Route path="/" element={<HomePage />} />
